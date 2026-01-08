38df68e5257c0812495e674d50d2481db78f3246
Hesham Salman <hsalman@makenotion.com>
2026-01-07 20:42:37 -0500

    Bump version to 0.7.0

--- Sources/SwiftCommitGen/SwiftCommitGen.swift
+++ Sources/SwiftCommitGen/SwiftCommitGen.swift
@@ -5,7 +5,7 @@ struct SwiftCommitGenCLI: AsyncParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "scg",
     abstract: "Generate AI-assisted Git commit messages using Apple's on-device models.",
-    version: "0.6.0",
+    version: "0.7.0",
     subcommands: [GenerateCommand.self, ConfigCommand.self],
     defaultSubcommand: GenerateCommand.self
   )
